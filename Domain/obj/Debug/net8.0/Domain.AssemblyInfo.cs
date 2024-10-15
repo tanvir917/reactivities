eb8b0c3c5feff652dbf6f6f1c74540794c98a3a5
Tanvir Islam <mdtanvirislams@gmail.com>
2024-10-16 00:02:29 +1100

    ef relationship - eagerly load

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a6a095a2592788eec6db8d56321a71877e56f96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b96c9aa392c665c0a414587499e54cfff985146b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
